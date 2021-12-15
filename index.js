0c3bbc999e1430bbfb7ac2c194e5805d6f3d2a9a
dhirajgagrai <dhirajgagrai@gmail.com>
2021-12-15 22:46:29 +0530

    Add example.com as example & Add comments

--- index.js
+++ index.js
@@ -16,21 +16,25 @@ const puppeteer = require('puppeteer');
     return document.querySelector(element).innerText;
   }, element);
   
+  // Read file pogchamp.txt
   fs.readFile('pogchamp.txt', function(err, data){
-    if (err.code === 'ENOENT')
-      fs.writeFile('pogchamp.txt', scrape, (err) => {
-        console.log("Notification Created");
-        if (err)
-          console.log(err);
-      });
 
-    else if (err)
-      console.log(err);
+    // If file doesn't exist, create it
+    if (err)
+      if (err.code === 'ENOENT')
+        fs.writeFile('pogchamp.txt', scrape, (err) => {
+          console.log("Notification Created");
+          if (err)
+            console.log(err);
+        });
+      else
+        console.log(err);
 
     else {
       var buf = Buffer.from(scrape);
       let changed = Buffer.compare(data, buf);
-  
+
+      // If change detected, write contents to file
       if (changed !== 0) {
         fs.writeFile('pogchamp.txt', scrape, (err) => {
           console.log("Notify via Email");
