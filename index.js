a65f5f8e4d22996b70ae21c01a136d4eabd824d6
dhirajgagrai <dhirajgagrai@gmail.com>
2021-12-16 15:04:47 +0530

    Change interaction of JSON files (input files)

--- index.js
+++ index.js
@@ -1,51 +1,61 @@
-const { url } = require('./json/url.json');
-const { element } = require('./json/element.json');
-const fs = require('file-system');
+const fs = require('fs');
+const list = require('./json/notification.json');
 
 const puppeteer = require('puppeteer');
 
 (async () => {
   const browser = await puppeteer.launch({headless: true});
-  const page = await browser.newPage();
+  let promises = [];
 
-  await page.goto(url , {
-    timeout: 0
+  list.forEach((el, i) => {
+    promises[i] = new Promise(async (resolve, reject) => {
+      const page = await browser.newPage();
+
+      await page.goto(el.url , {
+        timeout: 0
+      });
+
+      const scrape = await page.evaluate((element) => {
+        return document.querySelector(element).innerText;
+      }, el.element);
+      
+      // Read file of this ID
+      fs.readFile("id" + el.id, (err, data) => {
+        // If file doesn't exist, create it
+        if (err) {
+          if (err.code === 'ENOENT')
+            fs.writeFile("id" + el.id, scrape, (err) => {
+              resolve("Notification Created for ID " + el.id);
+              if (err)
+                reject(err);
+            });
+          else reject(err);
+        }
+        else {
+          var buf = Buffer.from(scrape);
+          let changed = Buffer.compare(data, buf);
+
+          // If change detected, write contents to file
+          if (changed !== 0) {
+            fs.writeFile("id" + el.id, scrape, (err) => {
+              resolve("Notification sent to " + el.email);
+              if (err)
+                reject(err);
+            });
+          }
+          else resolve("No Change for ID " + el.id);
+        }
+      });
+
+    });
   });
 
-  const scrape = await page.evaluate((element) => {
-    return document.querySelector(element).innerText;
-  }, element);
-  
-  // Read file pogchamp.txt
-  fs.readFile('pogchamp.txt', function(err, data){
+  Promise.all(promises).then(data => {
+    data.forEach(data => {
+      console.log(data);
+    });
 
-    // If file doesn't exist, create it
-    if (err)
-      if (err.code === 'ENOENT')
-        fs.writeFile('pogchamp.txt', scrape, (err) => {
-          console.log("Notification Created");
-          if (err)
-            console.log(err);
-        });
-      else
-        console.log(err);
-
-    else {
-      var buf = Buffer.from(scrape);
-      let changed = Buffer.compare(data, buf);
-
-      // If change detected, write contents to file
-      if (changed !== 0) {
-        fs.writeFile('pogchamp.txt', scrape, (err) => {
-          console.log("Notify via Email");
-          if (err)
-            console.log(err);
-        });
-      }
-      else
-        console.log("No Change");
-    }
+    browser.close();
   });
 
-  await browser.close();
 })();
